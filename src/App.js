360b9026d9189e7fed12199f7c11ada6699758c4
Alex Marin <alecs.marin@yahoo.com>
2017-02-23 20:13:45 +0200

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
-
+import { Button } from 'react-bootstrap';
+const wellStyles = {maxWidth: 400, margin: '0 auto 10px'};
 class App extends Component {
+
+
   render() {
     return (
       <div className="App">
           <h2>Welcome to the eHealth mobile technologies meetup</h2>
           <h2>Select your role</h2>
-          <ul>
-              <li><button className="btn btn-md">Im a sensor</button></li>
-              <li><button className="btn btn-md">Im a eHealth app enegineer</button></li>
-              <li><button className="btn btn-md">Im a healthcare proffessional</button></li>
+          <ul className="well" style={wellStyles}>
+              <Button bsStyle="success" block>Im a sensor</Button>
+              <Button bsStyle="success" block>Im a eHealth app enegineer</Button>
+              <Button bsStyle="success" block>Im a healthcare proffessional</Button>
           </ul>
       </div>
     );
